836e3558bda1ad5a580a272a24d967c84df5c152
David <vasqued2@yahoo.com>
2024-08-18 13:02:45 -0800

    Remove style from post card

--- dist/render_post.js
+++ dist/render_post.js
@@ -5,28 +5,6 @@ import { html, styleMap } from "https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-a
 export function renderPost(c) {
     // Render the HTML template using the provided object `c`
     const htmlTemplate = html`
-    <style>
-        .card { position: relative; overflow: hidden; padding: 16px 16px 20px; font-weight: 400; border-radius: var(--ha-card-border-radius, 10px); }
-        .title { text-align: center; font-size: 1.2em; font-weight: 500; }
-        .team-bg { opacity: 0.08; position: absolute; top: -20%; left: -20%; width: 58%; z-index: 0; }
-        .opponent-bg { opacity: 0.08; position: absolute; top: -20%; right: -20%; width: 58%; z-index: 0; }
-        .card-content { display: flex; justify-content: space-evenly; align-items: center; text-align: center; position: relative; z-index: 1; }
-        .team { text-align: center; width: 35%; }
-        .logo { max-height: 6.5em; }
-        .team img { max-width: 90px; }
-        .score { font-size: ${c.scoreSize}; text-align: center; line-height: 1; }
-        .score1op { opacity: ${c.scoreOp[1]}; }
-        .score2op { opacity: ${c.scoreOp[2]}; }
-        .divider { font-size: 2.5em; text-align: center; opacity: 0; }
-        .name { font-size: 1.4em; margin-bottom: 4px; }
-        .rank { font-size:0.8em; display: ${c.rankDisplay}; }
-        .post-series-info { display:${c.seriesSummaryDisplay}; font-size: 1.2em; text-align: center; margin: 4px; }
-        .post-row1 { font-size: 1.2em; text-align: center; }
-        .left-clickable { text-decoration: none; color: inherit; }
-        .right-clickable { text-decoration: none; color: inherit; }
-        .bottom-clickable { text-decoration: none; color: inherit; }
-        .disabled { pointer-events: none; cursor: default; }
-    </style>
     <ha-card>
         <div class="card">
             <div class="title">${c.title}</div>
