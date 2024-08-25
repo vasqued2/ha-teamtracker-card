0d74cf4f441f24dc60df8220f16489f03379215c
David <vasqued2@yahoo.com>
2024-08-25 05:47:00 -0800

    Use a local lit bundle

--- dist/render_pre.js
+++ dist/render_pre.js
@@ -1,4 +1,4 @@
-import { html, styleMap } from "https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js";
+import { html, styleMap } from "./lit/lit-all.min.js";
 
 
 // Define the rendering function
