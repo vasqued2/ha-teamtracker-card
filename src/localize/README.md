e78b5e6fcb0510534621c2ed1b352f8caaa1b4b2
David <vasqued2@yahoo.com>
2024-08-31 11:53:44 -0400

    v0.14.5 (#106)
    * Update version
    
    * de.js (#105)
    
    * improve formatting (#104)
    
    ---------
    
    Co-authored-by: Andreas Brett <andreasbrett@users.noreply.github.com>

--- src/localize/README.md
+++ src/localize/README.md
@@ -1,8 +1,8 @@
 To add a new language
-1. Copy the en.js file to the file for the new language.
+1. Copy the `en.js` file to the file for the new language.
 2. Make the following updates to the new file:
-   a. Change the variable name to the name of the new language
-   b. Translate the phrases
-3. Make the following change to the translator.js file
-   a. Import the new language file.
-   b. Add the new language to the languages object.
+   - Change the variable name to the name of the new language
+   - Translate the phrases
+3. Make the following change to the `translator.js` file
+   - Import the new language file.
+   - Add the new language to the languages object.
---
+++ src/localize/languages/de.js
@@ -0,0 +1,148 @@
+export const de = {
+    "common": {
+        "api_error": "API Error",
+        "no_upcoming_games": "Keine anstehenden Spiele bis %s",
+        "finalTerm": "%s - Endergebnis",
+        "byeTerm": "BYE",
+        "tourney2": "Finale",
+        "tourney4": "Halbfinale",
+        "tourney8": "Viertelfinale",
+        "tourney16": "Achtelfinale",
+        "tourney32": "Sechzehntelfinale",
+        "tourney64": "Vorrunde",
+        "tourney128": "Vorrunde",
+        "tourney256": "Vorrunde",
+        "today": "Heute",
+        "tomorrow": "Morgen"
+    },
+    "australian-football": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "baseball": {
+        "startTerm": "First Pitch",
+        "overUnder": "O/U: %s",
+        "gameStat1": "Balls %s",
+        "gameStat2": "Strikes %s",
+        "gameStat3": "%s Outs",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "basketball": {
+        "startTerm": "Tipoff",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "cricket": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "football": {
+        "startTerm": "Kickoff",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "golf": {
+        "startTerm": "Round Starts",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Shots (Thru)",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "hockey": {
+        "startTerm": "Puck Drop",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Torschüsse",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "mma": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    },
+    "racing": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Runden",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "soccer": {
+        "startTerm": "Anstoß",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Torschüsse",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "tennis": {
+        "startTerm": "First Serve",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "%s",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "volleyball": {
+        "startTerm": "Aufschlag",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "%s Score",
+        "teamBarLabel": "%s",
+        "oppoBarLabel": "%s"
+    },
+    "default": {
+        "startTerm": "Start",
+        "overUnder": "O/U: %s",
+        "gameStat1": "%s",
+        "gameStat2": "%s",
+        "gameStat3": "",
+        "gameBar": "Gewinnchance",
+        "teamBarLabel": "%s%",
+        "oppoBarLabel": "%s%"
+    }
+}
